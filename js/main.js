4cf8b1743acf996a49ae7520caf7edb4a1c1cf8b
Christopher Munoz <christopherm0507@gmail.com>
2023-03-07 22:51:04 -0500

    fixed cors errors

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://simple-rapper-api.herokuapp.com/api/${rapperName}`)
+        const response = await fetch(`https://git.heroku.com/simple-rap-api-chrismunozcodes.git/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
